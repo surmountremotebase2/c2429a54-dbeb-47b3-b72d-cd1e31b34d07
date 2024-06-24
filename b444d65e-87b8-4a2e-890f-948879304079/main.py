bacab2f6f79316e691d29a1274964f1080b5c858
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-06-24 10:44:30 -0400

    User Justin Graves saved changes to multiple files files at 06/24/2024, 14:44:29 UTC time.

--- b444d65e-87b8-4a2e-890f-948879304079/main.py
+++ b444d65e-87b8-4a2e-890f-948879304079/main.py
@@ -1,1 +1,30 @@
-#Type code here
+from surmount.base_class import Strategy, TargetAllocation
+from surmount.technical_indicators import SMA
+from surmount.logging import log
+
+class TradingStrategy(Strategy):
+
+   def __init__(self):
+      self.alloc = {}
+
+   @property
+   def assets(self):
+      return ["SVXY"]
+
+   @property
+   def interval(self):
+      return "1hour"
+
+   def run(self, data):
+      if len(data["ohlcv"]) == 0: 
+         self.alloc = {"SVXY":0}
+         return TargetAllocation(self.alloc)
+      data1 = data["ohlcv"][-1]["SVXY"]
+      if "14:00" in data1["date"]:
+         self.alloc = {"SVXY":0}
+         return TargetAllocation(self.alloc)
+      high = data1["high"]
+      low = data1["low"]
+      mid = high + low / 2
+      if data1["close"] <= mid: return TargetAllocation({"SVXY":1})
+      return TargetAllocation({"SVXY":0})
